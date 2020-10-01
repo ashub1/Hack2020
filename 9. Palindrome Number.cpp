aa4d093c78358d825ec925a169123cb8aa134f4b
Malathi Bhat <32553355+ashub1@users.noreply.github.com>
2020-10-02 00:25:02 +0530

    Codes Added.

---
+++ 9. Palindrome Number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x<0 || (x%10==0 && x!=0)) return false;
+        int ans=0;
+        while(x>ans)
+        {
+            ans = ans*10 + x%10;
+            x/=10;
+        }
+
+        return ans==x || ans/10==x;
+    }
+    
+};
